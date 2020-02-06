d137a9f82ecd4656873f227988ccd8718e7ecfc5
Carson <jackparadise520a@gmail.com>
2020-02-06 16:53:54 +0800

    add files for easier publishing and testing on PyPI

---
+++ merge.history.rst
@@ -0,0 +1,3 @@
+.. csv-table:: Merge-History
+   :header: Date, Version, "branch name", Discription
+   :widths: 10, 10, 20, 100
---
+++ pip_install_test.bat
@@ -0,0 +1,3 @@
+@echo off
+pip3 install --index-url https://test.pypi.org/simple/ "carson-ref.bezier==0.0.0.dev3"
+cmd.exe %~dp0
